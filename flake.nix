6ece9c96a3287da6d4b525548bd387c1448844e1
Maximilian Kienitz <max@kienitz.dev>
2025-01-05 05:12:19 +0100

    feat(flake): move to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,111 +1,103 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    fenix = {
-      url = "github:nix-community/fenix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs @ {
-    self,
-    crane,
-    devshell,
-    fenix,
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    {
-      nixosModules.bipper = import ./nix/module.nix inputs;
-      nixosModules.default = self.nixosModules.bipper;
-      overlays.default = final: prev: {
-        bipper = self.packages.${prev.stdenv.hostPlatform.system}.default;
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            devshell.overlays.default
-            fenix.overlays.default
-          ];
-        };
-        # Crane build
-        rustToolchain = pkgs.fenix.complete.withComponents [
-          "cargo"
-          "clippy"
-          "rustfmt"
-          "rust-src"
-          "rustc"
-        ];
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-        migrationsFilter = path: _type: builtins.match ".*/migrations/.*$" path != null;
-        cargoFilter = craneLib.filterCargoSources;
-        srcFilter = path: type: builtins.any (f: f path type) [cargoFilter migrationsFilter];
-        src = nixpkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter = srcFilter;
-        };
-        commonArgs = {
-          inherit src;
-          buildInputs =
-            [
-              # Add additional build inputs here
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              # Additional darwin specific inputs can be set here
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-              pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-              pkgs.libiconv
-              # pkgs.SDL2
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.nci.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          config,
+          ...
+        }:
+        let
+          crateName = "bipper";
+          projectName = crateName;
+          crateOutput = config.nci.outputs.${crateName};
+        in
+        {
+          devshells.default = {
+            packages = [
+              pkgs.nil
+              pkgs.rust-analyzer
             ];
+            devshell.startup.pre-commit.text = config.pre-commit.installationScript;
+          };
+
+          pre-commit.settings.hooks.treefmt.enable = true;
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              statix.enable = true;
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+
+          nci = {
+            projects.${projectName} = {
+              path = ./.;
+              numtideDevshell = "default";
+            };
+            crates.${crateName} = { };
+            # migrationsFilter = path: _type: builtins.match ".*/migrations/.*$" path != null;
+            # srcFilter = path: type: builtins.any (f: f path type) [cargoFilter migrationsFilter];
+          };
+
+          packages.default = crateOutput.packages.release;
+
+          overlayAttrs.coffee-labeler = config.packages.default;
         };
-        bipper = craneLib.buildPackage commonArgs;
-        # fpkgs = fenix.packages.${system};
-      in {
-        # nix flake check
-        checks = {
-          inherit bipper;
+
+      flake =
+        {
+          config,
+          pkgs,
+          self,
+          ...
+        }:
+        {
+          nixosModules = {
+            bipper = import ./nix/module.nix inputs;
+            default = config.nixosModules.bipper;
+          };
+          nixosTests.bipper = import ./nix/tests.nix { inherit pkgs self; };
         };
-        nixosTests.bipper = import ./nix/tests.nix {inherit pkgs self;};
-        packages.default = bipper;
-        packages.bipper-docker = pkgs.dockerTools.buildLayeredImage {
-          name = "bipper";
-          config.Cmd = ["${bipper}/bin/bipper"];
-        };
-        formatter = pkgs.alejandra;
-        devShells.default = pkgs.devshell.mkShell {
-          language.rust.enableDefaultToolchain = false;
-          imports = [
-            "${devshell}/extra/language/rust.nix"
-            "${devshell}/extra/language/c.nix"
-          ];
-          packages = with pkgs;
-            [
-              nil
-              rustToolchain
-              rust-analyzer
-            ]
-            ++ commonArgs.buildInputs;
-          env = [
-            {
-              name = "RUST_SRC_PATH";
-              value = "${rustToolchain}/lib/rustlib/src/rust/library";
-              # value = "/Users/max/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library";
-            }
-          ];
-        };
-      }
-    );
+    };
 }

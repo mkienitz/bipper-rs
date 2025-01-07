3e58b584ba2411103219df75a03158e1dbb8c879
Maximilian Kienitz <max@kienitz.dev>
2025-01-07 02:48:29 +0100

    chore: delete obsolete frontend

--- frontend/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-<div class="flex flex-col p-3 items-center justify-center w-screen h-screen">
-	<h1 class="flex p-10 text-4xl h-fit w-fit italic font-mono">BIPPER</h1>
-	<slot />
-</div>
